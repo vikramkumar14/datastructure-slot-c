0340b6f5d7bcc698a03ad96ef6bf20d89e5870e7
vikramkumarphython <123940240+vikramkumarphython@users.noreply.github.com>
2023-08-23 20:04:33 +0530

    Add files via upload

---
+++ Linked List.c
@@ -0,0 +1,113 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct node
+{
+	int info;
+	struct node *link;
+};
+
+void displayList(struct node *start);
+void search(struct node *start,int x);
+struct node *insertInOrder(struct node *start,int data);
+
+main()
+{
+	int choice,data;
+	struct node *start=NULL;
+
+	while(1)
+	{
+		printf("1.Display List\n");
+		printf("2.Insert a new node\n");
+		printf("3.Search\n");
+		printf("4.Exit\n");
+		printf("Enter your choice : ");
+		scanf("%d",&choice);
+		if(choice==4)
+			 break;
+		
+		switch(choice)
+		{
+		 case 1:
+			displayList(start);
+			break;
+		 case 2:
+			printf("Enter the element to be inserted : ");
+			scanf("%d",&data);
+			start=insertInOrder(start,data);
+			break;
+		 case 3:
+			printf("Enter the element to be searched : ");
+			scanf("%d",&data);
+			search(start,data);
+			break;
+		  default:
+			printf("Wrong choice\n");
+		}
+	}
+}
+
+struct node *insertInOrder(struct node *start,int data)
+{
+	struct node *p,*temp;
+	temp=(struct node *)malloc(sizeof(struct node));
+	temp->info=data;
+	if(start==NULL || data<start->info)
+	{
+		temp->link=start;
+		start=temp;
+		return start;
+	}
+
+	p=start;
+	while(p->link!=NULL && p->link->info <= data)
+		p=p->link;
+
+	temp->link=p->link;
+	p->link=temp;
+	return start;
+}
+
+
+void search(struct node *start,int x)
+{
+	struct node *p;
+	int position;
+	
+	if(start==NULL)
+	{
+		printf("List is empty\n");
+		return;
+	}
+
+	p=start;
+	position=1;
+	while(p!=NULL && p->info<=x)
+	{
+		if(p->info==x)
+			break;
+		position++;
+		p=p->link;
+	}
+	if(p==NULL || p->info!=x)
+		printf("%d not found in list\n",x);
+	else
+		printf("%d is at position %d\n",x,position);	
+}
+void displayList(struct node *start)
+{
+	struct node *p;
+	if(start==NULL)
+	{
+		printf("List is empty\n");
+		return;
+	}
+	printf("List is :\n");
+	p=start;
+	while(p!=NULL)
+	{
+		printf("%d\n",p->info);
+		p=p->link;
+	}
+	printf("\n\n");
+}
